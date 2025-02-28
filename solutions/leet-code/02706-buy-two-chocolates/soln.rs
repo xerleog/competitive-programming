295b84ec4002392b156ae34765289a1040df3ef1
xerleog <xerleog@gmail.com>
2025-02-28 08:42:25 -0800

    rust

---
+++ solutions/leet-code/02706-buy-two-chocolates/soln.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn buy_choco(mut prices: Vec<i32>, money: i32) -> i32 {
+        prices.sort();
+        if prices[0]+prices[1]<=money
+        {   money-prices[0]-prices[1]}
+        else
+        {   money}
+    }
+}
