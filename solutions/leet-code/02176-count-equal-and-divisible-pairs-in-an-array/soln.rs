073bbfeba3d77e3a08f3025cd18ee4141a71e794
xerleog <xerleog@gmail.com>
2025-04-17 08:58:04 -0700

    rust

---
+++ solutions/leet-code/02176-count-equal-and-divisible-pairs-in-an-array/soln.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let mut ans = 0;
+        for i in 0..nums.len()-1
+        {
+            for j in i+1..nums.len()
+            {
+                if nums[i]==nums[j] && (i*j)%k as usize==0
+                {
+                    ans+=1;
+                }
+            }
+        }
+        ans
+    }
+}
