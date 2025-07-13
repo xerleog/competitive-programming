3846dc917cf2cd72bb8c0362b03765bffaca1c63
xerleog <xerleog@gmail.com>
2025-07-13 08:20:02 -0700

    Delete solutions/leet-code/01812-determine-color-of-a-chessboard-square directory/

--- solutions/leet-code/01812-determine-color-of-a-chessboard-square/soln.rs
+++
@@ -1,6 +0,0 @@
-impl Solution {
-    pub fn square_is_white(coordinates: String) -> bool {
-        let val = coordinates.as_bytes().into_iter().collect::<Vec<_>>();
-        ((val[0]-97)^(val[1]-48))&1==0
-    }
-}
