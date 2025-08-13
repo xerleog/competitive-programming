c0e919928960d58386d95a625c98cf2123e8cd79
xerleog <xerleog@gmail.com>
2025-08-13 07:04:07 -0700

    rust

---
+++ solutions/leet-code/00326-power-of-three/soln3.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn is_power_of_three(mut n: i32) -> bool {
+       if n<=0 { return false;}
+       while n%3==0
+       {
+            n/=3;
+       } 
+       n==1
+    }
+}
