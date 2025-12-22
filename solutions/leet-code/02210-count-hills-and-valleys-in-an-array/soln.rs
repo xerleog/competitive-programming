633a7c38ef7935155b3ccb26fb67e6a87e5a3c5d
xerleog <xerleog@gmail.com>
2025-12-22 04:22:20 -0800

    rust

---
+++ solutions/leet-code/02210-count-hills-and-valleys-in-an-array/soln.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn count_hill_valley(mut nums: Vec<i32>) -> i32 {
+        nums.dedup();
+        let is_hill = |i| nums[i] > nums[i-1] && nums[i] > nums[i+1];
+        let is_valley = |i| nums[i] < nums[i-1] && nums[i] < nums[i+1];
+        (1..nums.len()-1)
+            .filter(|&i| is_hill(i) || is_valley(i))
+            .count() as _
+    }
+}
