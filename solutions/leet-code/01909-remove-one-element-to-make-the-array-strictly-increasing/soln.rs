18e388021c6f244d3022a3018c41c3c535ef118f
xerleog <xerleog@gmail.com>
2026-04-21 08:40:08 -0700

    rust

---
+++ solutions/leet-code/01909-remove-one-element-to-make-the-array-strictly-increasing/soln.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn can_be_increasing(nums: Vec<i32>) -> bool {
+        let len_n = nums.len();
+        let mut cnt = 0;
+        for idx in 1..len_n{
+            if nums[idx - 1] >= nums[idx]{
+                cnt += 1;
+                if cnt > 1{ return false;}
+                if (idx >= 2 && nums[idx - 2] >= nums[idx]) && (idx + 1 < len_n && nums[idx - 1] >= nums[idx + 1]){
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+ 
